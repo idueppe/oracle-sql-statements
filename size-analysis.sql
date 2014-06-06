1cdaf751b36521c006dc1d0ef3331dffdd744eec
idueppe <ingo.dueppe@dueppe.com>
2014-06-06 10:32:00 +0200

    enhanced sqls

--- size-analysis.sql
+++ size-analysis.sql
@@ -1,3 +1,42 @@
+
+-- Total Size of the current tablespace
+
+SELECT round(sum(bytes)/1024/1024/1024,2) as TOTAL_SIZE_IN_GB FROM user_segments;
+
+SELECT 
+  TABLESPACE_NAME, 
+  round(sum(bytes)/1024/1024/1024,2) as TOTAL_SIZE_IN_GB 
+FROM 
+  user_segments
+GROUP BY 
+  TABLESPACE_NAME;
+
+
+-- A monthly cluster of all data within a table
+-- The SQL needs a timestamp column when the row was created
+
+SELECT
+	month, 
+	anzahl, 
+	sum(anzahl) over (order by monat) kumuliert
+FROM
+	(SELECT monat, count(monat) as anzahl FROM
+	(SELECT to_char(creationDate,'YY MM') as monat FROM {TABLENAME})
+WHERE 
+	not monat is null
+GROUP BY 
+	monat
+HAVING 
+	count(monat) > 1
+ORDER BY 
+	monat ASC);
+
+
+-- Analyse the size of all table within the current user tablespace.
+-- The size contains the size of the table, assigned indexes, and lobsegments.
+-- The result also contains the totalsize in gigabytes and bytes and the oversize 
+-- The result is ordered by decreasing totalsize of the tables
+.
 SELECT
 	r.TABLE_NAME,
 	round (TOTALSIZE / 1024 / 1024 / 1024, 2) TOTALSIZE_GB,
@@ -26,7 +65,7 @@ FROM
 								SELECT
 									s.SEGMENT_NAME 		AS TABLE_NAME,
 									i.INDEX_NAME 		AS OBJECT_NAME,
-									'INDEX' 			AS OBJECT_TYPE,
+									seg.SEGMENT_TYPE	AS OBJECT_TYPE,
 									seg.BYTES 			AS SIZE_BYTES
 								FROM
 									user_segments s
@@ -36,7 +75,7 @@ FROM
 								SELECT
 									s.SEGMENT_NAME 		AS TABLE_NAME,
 									l.SEGMENT_NAME 		AS OBJECT_NAME,
-									'LOBS' 				AS OBJECT_TYPE,
+									seg.SEGMENT_TYPE	AS OBJECT_TYPE,
 									seg.BYTES 			AS SIZE_BYTES
 								FROM
 									user_segments s
